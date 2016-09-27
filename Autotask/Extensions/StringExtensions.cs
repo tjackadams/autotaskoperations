748a0440d619189886a8cefbbdcd436ecf2a37b9
Thomas Adams <tjackadams@itadams.co.uk>
2016-09-27 18:11:53 +0100

    Added the Autotask class library. Added the StringExtensions class with the Split() method. Added the Query class with the Generate method and overload method.

---
+++ Autotask/Extensions/StringExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Autotask.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="string"/> class
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Uses regex.Split method to split the given string by whitespaces.
+        /// This method will not split on doubles quotes"", which allows you
+        /// to pass in <see cref="DateTime"/> objects in double quotes "".
+        /// </summary>
+        public static string[] Split(this string str, RegexOptions options)
+        {
+            // split on ""
+            string regex = @"[ ](?=(?:[^""]*""[^""]*"")*[^""]*$)";
+
+            // define the regex
+            Regex myRegex = new Regex(regex, options);
+
+
+            var splitString = myRegex.Split(str);
+            List<string> returnArray = new List<string>();
+
+            // replace "" with empty string
+            for (int i = 0; i < splitString.Count(); i++)
+            {
+                var s = splitString[i];
+
+                s = s.Replace(@"""", string.Empty);
+
+                returnArray.Add(s);
+            }
+
+            return returnArray.ToArray();
+        }
+    }
+}
---
+++ Autotask/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Autotask")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e53d257-26ae-48c7-aa9e-80ed55c216a5")]
