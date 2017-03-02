e48bc87232e52321318e146e8f0bca78d0ecb0a5
Thomas Adams <tjackadams@itadams.co.uk>
2017-03-02 21:08:25 +0000

    Updated build script to include tests. Updated version number

--- Build.ps1
+++ Build.ps1
@@ -59,6 +59,6 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-#exec { & dotnet test .\test\YOUR_TEST_PROJECT_NAME -c Release }
+exec { & dotnet test .\test\Autotask.Tests -c Release }
 
 exec { & dotnet pack .\src\Autotask -c Release -o .\artifacts --version-suffix=$revision } 
