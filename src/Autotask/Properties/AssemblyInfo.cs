d670b20a3fc1eb01b92ac3ce74e892eb2a7fd8ce
Thomas Adams <tjackadams@itadams.co.uk>
2017-03-02 21:22:44 +0000

    changed framework from standard to core

--- src/Autotask/Properties/AssemblyInfo.cs
+++ src/Autotask/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Autotask")]
@@ -13,7 +13,5 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e53d257-26ae-48c7-aa9e-80ed55c216a5")]
+[assembly: ComVisible(false)]
